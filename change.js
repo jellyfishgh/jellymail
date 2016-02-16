6be37ec49bd63be9a8e36e1e948adf5999f61673
Jellyfish <1129088994@qq.com>
2016-02-16 14:07:39 +0800

    new mails

--- change.js
+++ change.js
@@ -1,16 +1,54 @@
 var fs = require('fs');
 
-fs.readFile('./public/mail_list_app.json', function(err, data) {
-    if (err) throw err;
-    var json = JSON.parse(data);
-    var keys = Object.keys(json);
-    var arr = [];
-    keys.map(function(key) {
-        arr.push('@"' + key.substring(1) + '"');
+function change(system) {
+    var sys = whichSys(system);
+    var wrapper = new Wrapper(sys);
+    fs.readFile('./public/mail_list_app.json', function(err, data) {
+        if (err) throw err;
+        var json = JSON.parse(data);
+        var keys = Object.keys(json);
+        var arr = [];
+        keys.map(function(key) {
+            arr.push(wrapper.wrap(key.substring(1)));
+        });
+        var str = arr.join(",");
+        fs.writeFile('./public/' + sys + '_mails', arr.join(','), (err) => {
+            if (err) throw err;
+            console.log(sys + " is saved.");
+        });
     });
-    var str = arr.join(",");
-    fs.writeFile('./public/app_mails', arr.join(','), (err) => {
-        if(err) throw err;
-        console.log("It\'s saved.");
-    });
-});
+}
+
+function Wrapper(sys) {
+    this.wrap = sys === 'iOS' ? iWrap : aWrap;
+}
+
+function iWrap(value) {
+    return '@"' + value + '"';
+}
+
+function aWrap(value) {
+    return '"' + value + '"';
+}
+
+function whichSys(system) {
+    var sys;
+    switch (system) {
+        case 'i':
+        case 'iOS':
+        case 'ios':
+            sys = 'iOS';
+            break;
+        case 'a':
+        case 'Android':
+        case 'android':
+            sys = 'Android';
+            break;
+        default:
+            sys = 'Android';
+    }
+    return sys;
+}
+
+var args = process.argv.slice(2);
+change(args[0]);
--- routes/allmails.js
+++ routes/allmails.js
@@ -16,30 +16,39 @@ router.use('/', function(req, res, next) {
         });
         fs.readFile('./public/wrong_mail.json', function(err, data) {
             if (err) res.end(404);
-            var wm = JSON.parse(data.toString());
-            for (var i = 0; i < wm.length; i++) {
-                if (obj.hasOwnProperty(wm[i])) delete obj[wm[i]];
-                else {
-                    console.log(wm[i]);
+            filterMails('w', JSON.parse(data.toString()), obj);
+            fs.readFile('./public/mail_map_server.json', function(err, data) {
+                if (err) res.end(404);
+                filterMails('r', Object.keys(JSON.parse(data.toString())), obj);
+                var array = [];
+                for (var key in obj) {
+                    array.push({
+                        'key': key,
+                        'value': obj[key]
+                    });
                 }
-            }
-            var array = [];
-            for (var key in obj) {
-                array.push({
-                    'key': key,
-                    'value': obj[key]
+                array.sort(function(a, b) {
+                    return b.value - a.value;
                 });
-            }
-            array.sort(function(a, b) {
-                return b.value - a.value;
-            });
-            res.render('pages/allmails', {
-                "title": "邮件列表",
-                "thisYear": new Date().getFullYear(),
-                "mails": array
+                res.render('pages/allmails', {
+                    "title": "邮件列表",
+                    "thisYear": new Date().getFullYear(),
+                    "mails": array
+                });
             });
         });
     });
 });
+
+function filterMails(type, ms, obj) {
+    for (var i = 0; i < ms.length; i++) {
+        if (obj.hasOwnProperty(ms[i])) {
+            if (type === 'r') console.log(type + '-->' + ms[i]);
+            delete obj[ms[i]];
+        } else {
+            // console.log(type + '-->' + ms[i]);
+        }
+    }
+}
 
 module.exports = router;
