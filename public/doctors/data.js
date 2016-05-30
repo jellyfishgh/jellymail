859b55bea5e6ba0326a6d57b72325b357ce9b595
jellyfishgh <1129088994@qq.com>
2016-05-30 23:23:36 +0800

    classes table page

--- public/doctors/data.js
+++ public/doctors/data.js
@@ -1,67 +1,66 @@
 var teachers = ["张海燕", "何洁", "杨艳娟"],
-    time = ["09:30-10:30", "15:30-16:30"],
+    times = ["09:30-10:30", "15:30-16:30"],
     title = "2016年6月福永人民医院孕妇学校课程表",
     subTitle = "免费公益讲座",
     tableHeads = ["日期", "时间", "课程内容", "授课老师"],
     classes = [{
-        date: "20160603",
+        date: "3",
         time: 1,
         content: "怀孕后“吃”的学问",
         teacher: 0
     }, {
-        date: "20160608",
+        date: "8",
         time: 0,
         content: "科学“做”月子",
         teacher: 1
     }, {
-        date: "20160609",
+        date: "9",
         time: 1,
         content: "孕期常见身体不适及缓解方法",
         teacher: 2
     }, {
-        date: "20160610",
+        date: "10",
         time: 1,
         content: "科学产检——为宝贝健康护航",
         teacher: 0
     }, {
-        date: "20160615",
+        date: "15",
         time: 0,
         content: "母乳喂养",
         teacher: 1
     }, {
-        date: "20160616",
+        date: "16",
         time: 1,
         content: "孕期健康的生活方式",
         teacher: 2
     }, {
-        date: "20160617",
+        date: "17",
         time: 1,
         content: "准妈妈心灵 SPA",
         teacher: 0
     }, {
-        date: "20160622",
+        date: "22",
         time: 0,
         content: "新生儿护理",
         teacher: 1
     }, {
-        date: "20160623",
+        date: "23",
         time: 1,
         content: "妊娠期糖尿病的自我监护",
         teacher: 2
     }, {
-        date: "20160624",
+        date: "24",
         time: 1,
         content: "孕期体重管理和如何数胎动",
         teacher: 0
     }, {
-        date: "20160629",
+        date: "29",
         time: 0,
         content: "分娩镇痛",
         teacher: 1
     }, {
-        date: "20160630",
+        date: "30",
         time: 1,
         content: "高危妊娠离我们有多远？",
         teacher: 2
-    }]
-};
+    }];
